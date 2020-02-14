b6773626306696464d6f475c8598b8295ecc549d
Andriy Veremyeyev and Tiffany Siu <tiffany@>
2020-02-13 16:29:16 -0800

    add random location button and function

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   // Set initial map to current location if permission given
   mapper.getCurrentLocation();
 
-  // When input new location and click button
+  // When input new location and click button center map at that location
   $("#submit").click(function(event){
     event.preventDefault();
     (async () => {
@@ -19,12 +19,30 @@ $(document).ready(function(){
       let address = $("#address").val();
       const response = await mapper.getLocation(address);
 
-      // Get latitude and longitude from json
-      let lat = response.results[0].geometry.location.lat;
-      let lon = response.results[0].geometry.location.lng;
-
-      // Send coordinates to be displayed
-      mapper.getMap(lat, lon);
+      getCoordinates(response);
     })();
   });
+  
+  // Center map at random locations in array
+  $("#random").click(function(event){
+    event.preventDefault();
+    (async () => {
+      // Get random address from array
+      let array = ["New York City", "Seattle", "Portland", "San Francisco", "Tokyo", "Sydney", "London"];
+      let num = Math.floor(Math.random() * array.length);
+      let address = array[num];
+      const response = await mapper.getLocation(address);
+
+      getCoordinates(response);
+    })();
+  });
+
+  // Get latitude and longitude from json
+  function getCoordinates(response){
+    let lat = response.results[0].geometry.location.lat;
+    let lon = response.results[0].geometry.location.lng;
+
+    // Send coordinates to be displayed
+    mapper.getMap(lat, lon);
+  }
 });
